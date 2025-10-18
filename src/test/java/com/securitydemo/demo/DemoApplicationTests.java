016ff3575a91614d512b8817c58f35c185a9e2fe
Nipurn <nbhaal0805@gmail.com>
2025-10-19 02:35:53 +0530

    Init

---
+++ src/test/java/com/securitydemo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securitydemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
